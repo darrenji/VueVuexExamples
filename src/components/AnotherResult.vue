6b65abcc0958466dcb02ab17ada1f8ab25548b5a
darren <764190362@qq.com>
2017-10-27 09:09:21 +0800

    当需要调用getters中的方法的时候，使用mapGetters去映射出对应的属性

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,20 +1,17 @@
 <template>
     <div>
-        <p>Another Counter is: {{counter}}</p>
-        <p>Number of clicks: {{clicks}}</p>
+        <p>Another Counter is: {{doubleCounter}}</p>
+        <p>Number of clicks: {{stringCounter}}</p>
     </div>
 </template>
 
 
 <script>
+    import { mapGetters } from 'vuex';
     export default {
-        computed: {
-            counter(){
-                return this.$store.getters.doubleCounter;
-            },
-            clicks(){
-                return this.$store.getters.stringCounter;
-            }
-        }
+        computed: mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ])
     }
 </script>
