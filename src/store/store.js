2af53c908c23adc9d555d787458158a257ea1a47
darren <764190362@qq.com>
2017-10-27 10:38:22 +0800

    使用vuex的module来分类管理state，分文件管理state

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './modules/counter';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value:0
     },
     getters: {
-        doubleCounter: state => {
-            return state.counter * 2;
-        },
-        stringCounter: state => {
-            return state.counter + ' clicks';
-        },
         value: state => {
             return state.value;
         }
@@ -22,37 +16,17 @@ export const store = new Vuex.Store({
     mutations: {
         //mutation需要同步发生
         //mutation对state的改变都是同步发生的
-        //mutation只能同步改变state,但vuex为我们提供了Actions，这让我们做一些异步的动作，然后commit给mutations,好像是先来做一些异步的动作，然后通过commit的方式告诉mutation
-        
-        increment: (state,payload) => {
-            state.counter += payload;
-        },
-        decrement: (state, payload) => {
-            state.counter -= payload;
-        },
+        //mutation只能同步改变state,但vuex为我们提供了Actions，这让我们做一些异步的动作，然后commit给mutations,好像是先来做一些异步的动作，然后通过commit的方式告诉mutation        
         updateValue: (state, payload) => {
             state.value = payload;
         }
     },
-    actions: {
-        increment: (context, payload) => {
-            context.commit('increment', payload);
-        },
-        decrement: ({commit}, payload) => {
-            commit('decrement', payload);
-        },
-        asyncIncrement: ({commit}, payload) => {
-            setTimeout(()=>{
-                commit('increment', payload.by);
-            },payload.duration);
-        },
-        asyncDecrement: ({commit}, payload) => {
-            setTimeout(()=> {
-                commit('decrement', payload.by);
-            },payload.duration);
-        },
+    actions: {       
         updateValue({commit}, payload){
             commit('updateValue', payload);
         }
+    },
+    modules: {
+        counter
     }
 });
