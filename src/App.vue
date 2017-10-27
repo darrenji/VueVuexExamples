a7c80f5a5acb3eff8c6de454ee1261b3f7579f0d
darren <764190362@qq.com>
2017-10-27 10:14:43 +0800

    解决外界数据与state数据的同步问题

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
                 <hr>
                 <app-counter></app-counter>
                 <app-another-counter></app-another-counter>
+                <hr>
+                <input type="text" :value="value" @input="updateValue">
+                <p>{{ value }}</p>
             </div>
         </div>
     </div>
@@ -19,6 +22,16 @@
     import AnotherResult from './components/AnotherResult.vue';
     import AnotherCounter from './components/AnotherCounter.vue';
     export default {
+        computed: {
+            value (){
+                return this.$store.getters.value;
+            }
+        },
+        methods:{
+            updateValue(event){
+                this.$store.dispatch('updateValue', event.target.value);
+            }
+        },
         components: {
             appCounter: Counter,
             appResult: Result,
