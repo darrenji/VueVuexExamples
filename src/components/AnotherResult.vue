4e75eda424cb05c5388e300d2b799a781d1674bf
darren <764190362@qq.com>
2017-10-27 08:47:35 +0800

    增加了一个Result组件，但是有重复的部分

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Another Counter is: {{counter}}</p>
+</template>
+
+
+<script>
+    export default {
+        computed: {
+            counter(){
+                return this.$store.state.counter * 2;
+            }
+        }
+    }
+</script>
