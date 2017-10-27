1506c1d5be9d12b06b5b630826f4774bd80a005b
darren <764190362@qq.com>
2017-10-27 08:41:43 +0800

    使用centralized state

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -5,6 +5,10 @@
 
 <script>
     export default {
-        props: ['counter']
+        computed: {
+            counter(){
+                return this.$store.state.counter;
+            }
+        }
     }
 </script>
